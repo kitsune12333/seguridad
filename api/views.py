b35855a6880cc94b3f48c7e2d975b82d194dbf86
MrLucasFernandez <lucasfernandez.mu@gmail.com>
2024-06-26 22:54:04 -0400

    Version AWS

--- api/views.py
+++ api/views.py
@@ -1,31 +1,23 @@
-from rest_framework import viewsets
 from .serializer import UsuariosSerializer
 from .models import Usuario
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-import requests
-from rest_framework.utils import json
 from django.shortcuts import render
 from django.contrib.auth import authenticate
+import os
+from supabase import create_client, Client
 
-# Tokens por grupo
-GRUPO_TOKENS = {
-    "ventas": "token_ventas",
-    "adquisiciones": "token_adquisiciones",
-    "contabilidad": "token_contabilidad",
-    "despacho": "token_despacho",
-    "postventa": "token_postventa",
-    "proveedor": "token_proveedor",
-    "reporteria": "token_reporteria",
-    "seguridad": "token_seguridad",
-    "stock": "token_stock"
-}
+#Direccion SUPABASE
+url: str = os.environ.get("SUPABASE_URL","https://hedwmixgqtmxzppsucbn.supabase.co")
+key: str = os.environ.get("SUPABASE_KEY","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhlZHdtaXhncXRteHpwcHN1Y2JuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc3NzI1MDMsImV4cCI6MjAzMzM0ODUwM30.f9vEpMtNGIFU7xO11w51Ct9CDVFY78RndNnNg_ntseI")
+supabase: Client = create_client(url, key)
+
+
 
 class IndexView(APIView):
-    permission_classes = [IsAuthenticated]
-    
+    permission_classes = [IsAuthenticated] 
     def get(self, request, format=None):
         return render(request, 'api/index.html')
 
@@ -39,16 +31,22 @@ def RegisterUsuario(request):
 def UsuarioAdd(request):
     token = request.data.get('token')
     departamento = request.data.get('departamento')
+    
+    response = supabase.table("token_departamento").select("*").execute()
+    data_tokens = [
+            {'id': item['id'], 'departamento': item['departamento'], 'token': item['token']}
+            for item in response.data]
 
-    if GRUPO_TOKENS.get(departamento) == token:
-        serializer = UsuariosSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return render(request, 'api/login.html')
+    for i in data_tokens:
+        if i["departamento"] == departamento and i["token"] == token:
+            serializer = UsuariosSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return render(request, 'api/login.html')
+            else:
+                return Response(serializer.errors)
         else:
-            return Response(serializer.errors)
-    else:
-        return Response({"error": "Token inválido"}, status=400)
+            return Response({"error": "Token inválido"}, status=400)
 
 @api_view(["GET"])
 def ListarUsuarios(request):
@@ -61,13 +59,19 @@ def ValidarUsuario(request):
     username = request.POST.get("username")
     password = request.POST.get("password")
     token = request.POST.get("token")
-
+    
+    response = supabase.table("token_departamento").select("*").execute()
+    data_tokens = [
+            {'id': item['id'], 'departamento': item['departamento'], 'token': item['token']}
+            for item in response.data]
+    
     user = authenticate(username=username, password=password)
     if user is not None:
         departamento = user.departamento
-        if GRUPO_TOKENS.get(departamento) == token:
-            return render(request, 'api/index.html', {'user': user})
-        else:
-            return Response({"error": "Token inválido"}, status=400)
+        for i in data_tokens:
+            if i["departamento"] == departamento and i["token"] == token:
+                return render(request, 'api/index.html', {'user': user})
+            else:
+                return Response({"error": "Token inválido"}, status=400)
     else:
         return Response({"error": "Credenciales inválidas"}, status=400)
